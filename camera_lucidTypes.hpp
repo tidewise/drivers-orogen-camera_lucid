7150c9b10465d9a711dcdfdf1cb3c5dfc94a7e63
Afonso da Fonseca Braga <afonso.braga@engenharia.ufjf.br>
2023-01-24 11:35:49 -0300

    initial commit by rock-create-orogen

---
+++ camera_lucidTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef camera_lucid_TYPES_HPP
+#define camera_lucid_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace camera_lucid {
+}
+
+#endif
+
